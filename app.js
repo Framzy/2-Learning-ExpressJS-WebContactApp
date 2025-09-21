932b7160231e9ee404c09a8b6943ee3d7b040525
Framzy <fardenramzy97@gmail.com>
2025-09-21 18:58:45 +0700

    add about page

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const port = 3000;
@@ -6,6 +7,7 @@ const port = 3000;
 // Middleware
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 // View Engine
 app.set("view engine", "ejs");
@@ -15,10 +17,14 @@ app.get("/", (req, res) => {
   res.render("index", { title: "Web Contact App" });
 });
 
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About Page" });
+});
+
 app.post("/contact", (req, res) => {
   const { name, email, message } = req.body;
   console.log(`Pesan dari ${name} <${email}>: ${message}`);
-  res.send("Pesan berhasil dikirim!");
+  res.render("index", { title: "Web Contact App" });
 });
 
 app.listen(port, () => {
